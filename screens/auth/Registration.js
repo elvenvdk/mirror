09ae6a66040ccd8f03cfaad9c51846c191d00038
Alvin van der Kuech <avanderkuech@sprucetech.com>
2021-09-29 19:26:20 -0400

    Added registration

---
+++ screens/auth/Registration.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+const Registration = () => {
+  return (
+    <View>
+      <Text>Registration Screen</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default Registration;
