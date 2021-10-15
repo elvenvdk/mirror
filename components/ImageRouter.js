9f1f47ecd747c267923b8a5309b3009621615864
Alvin van der Kuech <avanderkuech@sprucetech.com>
2021-10-15 05:48:33 -0400

    Added icons

--- components/ImageRouter.js
+++ components/ImageRouter.js
@@ -3,6 +3,7 @@ import { StyleSheet, Text, View } from "react-native";
 
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Icon from "react-native-vector-icons/FontAwesome5";
 
 import ShutterButton from "../screens/image/ShutterButton";
 import Gallery from "../screens/image/Gallery";
@@ -12,9 +13,22 @@ const Tab = createBottomTabNavigator();
 
 const ImageRouter = ({ navigation }) => {
   return (
-    <Tab.Navigator initialRouteName="Shutter Button">
-      <Tab.Screen name="Shutter Button" component={ShutterButton} />
-      <Tab.Screen name="Image Gallery" component={Gallery} />
+    <Tab.Navigator
+      initialRouteName="Shutter Button"
+      screenOptions={{ tabBarShowLabel: false }}
+    >
+      <Tab.Screen
+        name="Shutter Button"
+        component={ShutterButton}
+        options={{
+          tabBarIcon: () => <Icon name="camera" size={30} />,
+        }}
+      />
+      <Tab.Screen
+        name="Image Gallery"
+        component={Gallery}
+        options={{ tabBarIcon: () => <Icon name="images" size={30} /> }}
+      />
     </Tab.Navigator>
   );
 };
